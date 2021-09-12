22ce8d4ffc901279b7e34b787454baf1394c8988
Your Name <you@example.com>
2021-09-12 19:57:58 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+function show()
+	{
+		alert("Welcome To Cook with Dusk!");
+	}
+
+	show();
+	
+
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+ 
+
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+         
+        
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+        menu.classList.add("active");
+         
+       
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+}
+ 
+toggle.addEventListener("click", toggleMenu, false);
+
+
+const items = document.querySelectorAll(".item");
+ 
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+}
+ 
+
+for (let item of items) {
+    if (item.querySelector(".submenu")) {
+      item.addEventListener("click", toggleItem, false);
+      item.addEventListener("keypress", toggleItem, false);
+    }   
+}
+
+function closeSubmenu(e) {
+  let isClickInside = menu.contains(e.target);
+ 
+  if (!isClickInside && menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  }
+}
+ 
+document.addEventListener("click", closeSubmenu, false);
